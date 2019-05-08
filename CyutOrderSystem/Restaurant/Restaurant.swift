97755410862e49a1dd076467d7fd2e1dad7e5cd4
yacheng <a0920119383@gmail.com>
2019-05-08 11:12:47 +0800

    First layout

---
+++ CyutOrderSystem/Restaurant/Restaurant.swift
@@ -0,0 +1,10 @@
+//
+//  Restaurant.swift
+//  CyutOrderSystem
+//
+//  Created by yacheng on 2019/4/20.
+//  Copyright © 2019 yacheng. All rights reserved.
+//
+
+import Foundation
+
---
+++ CyutOrderSystem/Restaurant/RestaurantModel.swift
@@ -0,0 +1,14 @@
+//
+//  RestaurantModel.swift
+//  CyutOrderSystem
+//
+//  Created by yacheng on 2019/4/26.
+//  Copyright © 2019 yacheng. All rights reserved.
+//
+
+import Foundation
+
+struct restaurantModel: Codable {
+    var name: String
+    var type: String
+}
